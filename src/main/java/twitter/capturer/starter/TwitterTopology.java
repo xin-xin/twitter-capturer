fe84f91816c0c84c1ded66395a64ea6178d4a592
Xin Xin <xinx@live.com.sg>
2017-09-26 17:13:56 +0800

    Clean code.

--- src/main/java/twitter/capturer/starter/TwitterTopology.java
+++ src/main/java/twitter/capturer/starter/TwitterTopology.java
@@ -20,35 +20,36 @@ public class TwitterTopology {
 	public static Map <String, Object> getServerConf() throws IOException {
         Yaml yaml = new Yaml();
         
-        InputStream _hdfsConfig = TwitterTopology.class.getResourceAsStream("/hdfs.config");
+        InputStream hdfsConfig = TwitterTopology.class.getResourceAsStream("/hdfs.config");
         @SuppressWarnings("unchecked")
-		Map<String, Object> conf = (Map<String, Object>) yaml.load(_hdfsConfig);           
-        _hdfsConfig.close();
+		Map<String, Object> conf = (Map<String, Object>) yaml.load(hdfsConfig);           
+        hdfsConfig.close();
         
         return conf;
 	}	
 	
 	public static void main(String[] args) throws Exception {	
 		
-		String TOPOLOGY_NAME = args[0];		
-		String SUB_FOLDER = args[1];
-		String[] QUEUE_INPUT = args[2].split(",");
-		String[] QUEUE_OUTPUT = args[3].split(",");
+		String topologyNAME = args[0];		
+		String subFOLDER = args[1];
+		String[] queueINPUT = args[2].split(",");
+		String[] queueOUTPUT = args[3].split(",");
+		String streamSpout = "stream_spout";
 		
 		TopologyBuilder builder = new TopologyBuilder();
 		Config conf = new Config();		
 	    conf.setNumWorkers(1);
         conf.put("hdfs.config", getServerConf());            
 	    
-        SaveHDFSBolt sb = new SaveHDFSBolt(SUB_FOLDER);
+        SaveHDFSBolt sb = new SaveHDFSBolt(subFOLDER);
         
-		builder.setSpout("stream_spout", new StreamSpout(QUEUE_INPUT));
+		builder.setSpout(streamSpout, new StreamSpout(queueINPUT));
 		builder.setBolt("save_bolt", sb.bolt()).shuffleGrouping("stream_spout");
-		builder.setBolt("queue_bolt", new QueueBolt(QUEUE_OUTPUT)).shuffleGrouping("stream_spout");
-		builder.setBolt("save_db_bolt", new SaveRDBMSBolt()).shuffleGrouping("stream_spout");
-		builder.setBolt("save_nosql_bolt", new SaveNOSQLBolt()).shuffleGrouping("stream_spout");
+		builder.setBolt("queue_bolt", new QueueBolt(queueOUTPUT)).shuffleGrouping(streamSpout);
+		builder.setBolt("save_db_bolt", new SaveRDBMSBolt()).shuffleGrouping(streamSpout);
+		builder.setBolt("save_nosql_bolt", new SaveNOSQLBolt()).shuffleGrouping(streamSpout);
 		
 //		Set up for production
-	    StormSubmitter.submitTopology(TOPOLOGY_NAME, conf, builder.createTopology());			    
+	    StormSubmitter.submitTopology(topologyNAME, conf, builder.createTopology());			    
 	}	
 }
--- src/main/java/twitter/capturer/twitter/TwitterListener.java
+++ src/main/java/twitter/capturer/twitter/TwitterListener.java
@@ -3,48 +3,50 @@ package twitter.capturer.twitter;
 import java.util.Date;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.apache.log4j.Logger;
+
 import data.common.domain.nosql.Tweet;
 import data.common.util.JsonUtil;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
-import twitter4j.json.DataObjectFactory;
+import twitter4j.TwitterObjectFactory;
 
-@SuppressWarnings("deprecation")
 public class TwitterListener implements StatusListener {
-	private LinkedBlockingQueue<String> _queue;
+	private Logger log = Logger.getLogger(this.getClass());
+	private LinkedBlockingQueue<String> queue;
 
 	public TwitterListener(LinkedBlockingQueue<String> queue) {
-		_queue = queue;
+		this.queue = queue;
 	}
 
 	public LinkedBlockingQueue<String> getQueue() {
-		return _queue;
+		return queue;
 	}
 
 	public void onException(Exception arg0) {
-		// TODO Auto-generated method stub
-
+		log.debug("onException: " + arg0.getMessage());
 	}
 
 	public void onDeletionNotice(StatusDeletionNotice arg0) {
-		// TODO Auto-generated method stub
-
+		log.debug("onDeletionNotice: " + arg0.getStatusId());
 	}
 
 	public void onScrubGeo(long arg0, long arg1) {
-		// TODO Auto-generated method stub
-
+		log.debug("onScrubGeo: " + arg0 + ", " + arg1);
 	}
 
 	public void onStallWarning(StallWarning arg0) {
-		// TODO Auto-generated method stub
+		log.debug("onStallWarning: " + arg0.getMessage());
+	}
 
+	public void onTrackLimitationNotice(int arg0) {
+		log.debug("onTrackLimitationNotice: " + arg0);
 	}
 
 	public void onStatus(Status status) {
-		String statusJson = DataObjectFactory.getRawJSON(status);
+		String statusJson = TwitterObjectFactory.getRawJSON(status);
 		Tweet tweet = JsonUtil.fromJson(statusJson, Tweet.class);
 		Date date = new Date();
 		tweet.setCaptured_at(date);
@@ -53,12 +55,7 @@ public class TwitterListener implements StatusListener {
 		tweet.setOrigin_code("SG");
 
 		statusJson = JsonUtil.toJson(tweet);
-		_queue.offer(statusJson);
+		boolean result = queue.offer(statusJson);
+		log.debug("queue offer result: " + result);
 	}
-
-	public void onTrackLimitationNotice(int arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
